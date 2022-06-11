996d6c484d26d5626d712b321b705859d86ce179
Kristina Vlasovets <kristinavlasovets@gmail.com>
2022-06-11 19:42:52 +0300

    added database

--- src/App.js
+++ src/App.js
@@ -2,34 +2,29 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Categories } from "./components/Categories";
 import { SortPopup } from "./components/SortPopup";
-import { BreadBlock} from "./components/BreadBlock";
+import { BreadBlock } from "./components/BreadBlock";
+
+import breads from "./db.json"
+
 
 import "./scss/app.scss";
 
-
-
 function App() {
+  console.log(breads.breads)
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-       <div className="container">
-         <div className="content__top">
-           <Categories />
-           <SortPopup/>
-         </div>
-         <h2 className="content__title">Products</h2>
-         <div className="content__items">
-         <BreadBlock title='Wheat' price='5'/>
-         <BreadBlock type='mixed'/>
-         <BreadBlock/>
-         <BreadBlock/>
-         <BreadBlock/>
-         <BreadBlock/>
-         <BreadBlock/>
-         <BreadBlock/>
-         </div>
-       </div>
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <SortPopup />
+          </div>
+          <h2 className="content__title">Products</h2>
+          <div className="content__items">
+            {breads && breads.breads.map((obj) => (<BreadBlock title={obj.title} price={obj.price} />))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(2);
+
+  const categories = ['All', 'French traditional', 'Organic', 'Sourdough'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  console.log(activeIndex);
   return (
     <div className="categories">
       <ul>
-        <li className="active">All</li>
-        <li>French traditional</li>
-        <li>Organic</li>
-        <li>Sourdough</li>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? 'active' : ''}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
